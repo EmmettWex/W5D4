60dd0975716f901e66f996a75bb95a802dba1014
Wilson Wu <wuyecheng738@gmail.com>
2022-12-08 17:27:06 -0500

    up to phase2

---
+++ URLShortener/db/migrate/20221208220814_create_shortened_urls.rb
@@ -0,0 +1,13 @@
+class CreateShortenedUrls < ActiveRecord::Migration[7.0]
+  def change
+    create_table :shortened_urls do |t|
+      t.string :long_url, null: false
+      t.string :short_url, null: false # Check later
+
+      t.timestamps
+    end
+
+    add_index :shortened_urls, :long_url
+    add_index :shortened_urls, :short_url, unique: true
+  end
+end
